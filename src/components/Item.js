ebed7d5b0bcdb62a15ebb8d5467596faafca2390
tanui-ivy <ivy.tanui@student.moringaschool.com>
2025-02-21 23:45:50 +0300

    State and Events lab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleCartToggle(){
+    setIsInCart((prevStatus) => !prevStatus);
+  }
+
+  const itemClass = isInCart? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick= {handleCartToggle}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+        </button>
     </li>
   );
 }
